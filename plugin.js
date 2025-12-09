64cb02f0251c833de4a032ae4a57240d9ea7ec7a
Andrew Lemos <156254579+andrewlemos@users.noreply.github.com>
2025-12-09 01:19:45 -0300

    Bump plugin version to 1.0.4

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 const __pluginConfig =  {
   "name": "windy-plugin-convective-tracker",
-  "version": "1.0.3",
+  "version": "1.0.4",
   "icon": "⚡",
   "title": "Convective Formation Tracker",
   "description": "Tracker para monitorar e prever deslocamento de formações convectivas",
